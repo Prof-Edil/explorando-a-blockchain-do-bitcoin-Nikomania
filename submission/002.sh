c7d452ad299774095be84104b5bbe420d29a9482
Thiago Carrijo <thiagocrrabelo@gmail.com>
2024-11-06 16:37:13 -0300

    Fix task 2

--- submission/002.sh
+++ submission/002.sh
@@ -3,8 +3,4 @@
 #   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
 
-address = "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
-message = "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
-signature = "HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
-
-bitcoin-cli verifymessage $address $signature $message
+bitcoin-cli verifymessage 1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM= 1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa
