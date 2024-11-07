5229ea4cb6a0938dd285fd00d6cd41812dcf1cff
Thiago Carrijo <thiagocrrabelo@gmail.com>
2024-11-06 22:52:16 -0300

    Task 3 completed

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,6 @@
 # How many new outputs were created by block 123,456?
+
+block_number=123456
+block_hash=$(bitcoin-cli getblockhash $block_number)
+block=$(bitcoin-cli getblock $block_hash)
+echo $block | jq '.tx' | jq length
