f9bb1ae8b844e4204dd6e9e8c41c84adb1738cb3
Thiago Carrijo <thiagocrrabelo@gmail.com>
2024-11-11 00:32:38 -0300

    Task 3 completed

--- submission/003.sh
+++ submission/003.sh
@@ -2,5 +2,8 @@
 
 block_number=123456
 block_hash=$(bitcoin-cli getblockhash $block_number)
-block=$(bitcoin-cli getblock $block_hash)
-echo $block | jq '.tx' | jq length
+block=$(bitcoin-cli getblock $block_hash 2) # verbosity=2
+
+vout_total_count=$(echo $block | jq '[.tx[] | .vout | length] | add')
+
+echo $vout_total_count
