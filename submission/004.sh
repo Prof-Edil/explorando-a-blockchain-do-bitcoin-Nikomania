90c3a5e96eb82a7ac5a90f837f7fdddfb6496307
Thiago Carrijo <thiagocrrabelo@gmail.com>
2024-11-17 19:23:48 -0300

    Fix Task 4

--- submission/004.sh
+++ submission/004.sh
@@ -1,9 +1,8 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
-pub_key_desc_simple="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)"
-
-descriptor_info=$(bitcoin-cli getdescriptorinfo $pub_key_desc_simple)
-checksum=$(echo $descriptor_info | jq '.checksum' | sed 's/"//g')
-adress=$(bitcoin-cli deriveaddresses "${pub_key_desc_simple}#${checksum}" "[100,100]" | jq '.[0]' | sed 's/"//g')
-echo $adress
+xpub_key="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+xpub_key_desc="tr($xpub_key/*)"
+checksum=$(bitcoin-cli getdescriptorinfo $xpub_key_desc | jq -r '.checksum')
+taproot_address=$(bitcoin-cli deriveaddresses "$xpub_key_desc#$checksum" "[100,100]" | jq -r '.[0]')
+echo $taproot_address
