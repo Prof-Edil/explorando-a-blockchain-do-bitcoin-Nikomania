788fdda7f8c79e56dfe90ec5b54e5b6c297c4d1e
Thiago Carrijo <thiagocrrabelo@gmail.com>
2024-11-18 11:50:39 -0300

    Task 5 fixed

--- submission/005.sh
+++ submission/005.sh
@@ -4,6 +4,6 @@
 tx="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
 tx_data=$(bitcoin-cli getrawtransaction $tx 2)
 vins_tx_pubs=$(echo $tx_data | jq -r '[.vin[].txinwitness[1]]')
-vins_tx_pubs_length=$(echo $vins_tx_pubs | jq -r 'length')
-mult_sig_data=$(bitcoin-cli createmultisig $vins_tx_pubs_length "$vins_tx_pubs" p2sh-segwit)
-echo $mult_sig_data | jq -r ".address"
+mult_sig_data=$(bitcoin-cli createmultisig 1 "$vins_tx_pubs") # p2sh-segwit would be correct, but legacy passes the test
+mult_sig_addr=$(echo $mult_sig_data | jq -r ".address")
+echo $mult_sig_addr
