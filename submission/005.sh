571d34a84549a28ee7200d04d7a63d40d9579f22
Thiago Carrijo <thiagocrrabelo@gmail.com>
2024-11-18 02:19:10 -0300

    Fix task 5

--- submission/005.sh
+++ submission/005.sh
@@ -3,10 +3,7 @@
 
 tx="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
 tx_data=$(bitcoin-cli getrawtransaction $tx 2)
-echo $tx_data | jq -r
 vins_tx_pubs=$(echo $tx_data | jq -r '[.vin[].txinwitness[1]]')
-echo $vins_tx_pubs
 vins_tx_pubs_length=$(echo $vins_tx_pubs | jq -r 'length')
-echo $vins_tx_pubs_length
 mult_sig_data=$(bitcoin-cli createmultisig $vins_tx_pubs_length "$vins_tx_pubs" p2sh-segwit)
 echo $mult_sig_data | jq -r ".address"
